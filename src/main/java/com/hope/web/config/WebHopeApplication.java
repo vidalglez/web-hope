f1b407c517376d252e433639a9e8980771696fdd
Vidal Gonzalez <darkvidal@gmail.com>
2021-10-07 23:23:10 -0500

    feture: add init controller

--- src/main/java/com/hope/web/config/WebHopeApplication.java
+++ src/main/java/com/hope/web/config/WebHopeApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.hope.web.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WebHopeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WebHopeApplication.class, args);
 	}
 
 }
