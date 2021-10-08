9f66dafd602fe5d915852fce867c4571d28366bc
Vidal Gonzalez <darkvidal@gmail.com>
2021-10-08 00:14:29 -0500

    refactor: change controller package

--- src/main/java/com/hope/web/WebHopeApplication.java
+++ src/main/java/com/hope/web/WebHopeApplication.java
@@ -1,4 +1,4 @@
-package com.hope.web.config;
+package com.hope.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/hope/web/WebHopeApplicationTests.java
+++ src/test/java/com/hope/web/WebHopeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hope.web.config;
+package com.hope.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
