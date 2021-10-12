79d2026517155eea5d0b50899b96a68df9972002
Vidal Gonzalez <darkvidal@gmail.com>
2021-10-11 21:44:11 -0500

    test: adding initial controller unit test

--- src/test/java/com/hope/web/WebHopeApplicationTests.java
+++ src/test/java/com/hope/web/WebHopeApplicationTests.java
@@ -1,13 +1,21 @@
 package com.hope.web;
 
+import com.hope.web.controller.InitialController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class WebHopeApplicationTests {
 
+	@Autowired
+	private InitialController initialController;
+
 	@Test
 	void contextLoads() {
+		assertThat(initialController).isNotNull();
 	}
 
 }
